ccb1fba92bef23ecab7a6646a56564074430c684
Asmo Korkiatupa <Smonk7@gmail.com>
2015-03-09 19:52:05 +0000

    Src kansio gittiin 	new file:   src/css/mystyle.css 	new file:   src/php/testi.php

---
+++ src/php/testi.php
@@ -0,0 +1,13 @@
+ <head>
+<link rel="stylesheet" type="text/css" href="../css/mystyle.css">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+</head>
+<body>
+
+<?php
+echo "<h1>Admin Panel</h1>"; 
+echo "<p>jee!</p>";
+?>
+</body>
