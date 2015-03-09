e9107add708b0b38a490e8a23a3796fde1d2611d
Asmo Korkiatupa <Smonk7@gmail.com>
2015-03-09 21:09:33 +0000

    Kansiorakennetta uusiksi, ja sivuston navigointi luotu 	new file:   src/php/header.php 	new file:   src/php/views/adminSettings.php 	new file:   src/php/views/databaseManagement.php 	new file:   src/php/views/options.php 	new file:   src/php/views/status.php 	new file:   src/php/views/userManagement.php 	deleted:    src/php/testi.php

---
+++ src/php/header.php
@@ -0,0 +1,6 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../css/mystyle.css">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
---
+++ src/php/views/databaseManagement.php
@@ -0,0 +1,26 @@
+<?php include '../header.php';?>
+<div data-role="page" data-theme="b">
+<body>
+
+<div data-role="header" data-theme="b" id="navtop">
+    <h1>Admin Panel</h1>
+	 <a href="options.php" class="ui-btn-right ui-btn ui-btn-inline ui-mini ui-corner-all ui-btn-icon-right ui-icon-gear">Options</a>
+	 <div data-role="navbar" data-grid="c" data-theme="b">
+    <ul>
+        <li><a href="status.php" >Status</a></li>
+        <li><a href="userManagement.php" >User Management</a></li>
+        <li><a href="databaseManagement.php" class="ui-btn-active">Database Management</a></li>
+        <li><a href="adminSettings.php">Admin Settings</a></li>
+    </ul>
+</div><!-- /navbar -->
+</div>
+
+<div id="container" data-role="main" class="ui-content">
+
+<h1>Database Management</h1>
+<?php
+echo "<p>I Am Now A Mobile Developer!!</p>";
+?>
+</div>
+</div>
+</body>
