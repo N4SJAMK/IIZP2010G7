b9a76b09ce3954828822b596dc0eb88e11826762
Asmo Korkiatupa <Smonk7@gmail.com>
2015-03-09 20:30:52 +0000

    Lets get this party started, UI tehty. 	modified:   src/css/mystyle.css 	modified:   src/php/testi.php

--- src/php/testi.php
+++ src/php/testi.php
@@ -1,13 +1,31 @@
- <head>
+<head>
 <link rel="stylesheet" type="text/css" href="../css/mystyle.css">
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
-<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 </head>
+<div data-role="page" data-theme="b">
 <body>
 
+<div data-role="header" data-theme="b" id="navtop">
+    <h1>Admin Panel</h1>
+	 <a href="#" class="ui-btn-right ui-btn ui-btn-inline ui-mini ui-corner-all ui-btn-icon-right ui-icon-gear">Options</a>
+	 <div data-role="navbar" data-grid="c" data-theme="b">
+    <ul>
+        <li><a href="#" class="ui-btn-active">Status</a></li>
+        <li><a href="#">User Management</a></li>
+        <li><a href="#">Database Management</a></li>
+        <li><a href="#">Admin Settings</a></li>
+    </ul>
+</div><!-- /navbar -->
+</div>
+
+<div id="container" data-role="main" class="ui-content">
+
+<h1>Status</h1>
 <?php
-echo "<h1>Admin Panel</h1>"; 
-echo "<p>jee!</p>";
+echo "<p>I Am Now A Mobile Developer!!</p>";
 ?>
+</div>
+</div>
 </body>
