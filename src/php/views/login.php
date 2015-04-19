76f987dfce2e09a76a8dd6f324dfa73fe5bc57c9
Ali <ali_aldool@yahoo.com>
2015-04-19 23:58:07 +0300

    Few modifications

--- src/php/views/login.php
+++ src/php/views/login.php
@@ -1,25 +1,5 @@
 <?php include '../../../mongo/db-init.php';?>
 <?php include '../../../mongo/functions.php';?>
-
-<?php
-session_start();
-
-if (isset($_POST['email'])) {
-  $admins = findAll($admins);
-  foreach ($admins as $admin) {
-    if ($_POST['email'] == $admin['email']) {
-		
-      if ($_POST['password'] == $admin['password']) {
-        $_SESSION['loggedIn'] = true;
-		$_SESSION['email'] = $admin['email'];
-        header('Location: status.php');
-      }
-    }
-  }
-}
-
-?>
-
 <head>
 <link rel="stylesheet" type="text/css" href="../../css/mystyle.css">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
@@ -38,24 +18,35 @@ if (isset($_POST['email'])) {
 </div>
 
 <div id="container" data-role="main" class="ui-content">
-
 <h1 style="text-align: center;">Login</h1>
-
-<?php
-if (isset($_POST['email'])) {
-  echo "<h2 style='text-align: center;'>Väärä käyttäjätunnus tai salasana!</h2>";
-}
-?>
 
   <form method='POST' action='login.php'>
     <div class ="loginform" style="text-align: center; max-width: 400px; margin-left: auto; margin-right: auto;">
-      <input name="email" id="textinput-fc" type="text" value="" placeholder="Sähköposti">
-      <input name="password" id="textinput-fc" type="password" value="" placeholder="Salasana">
-      <input type="submit" value="Ok">
+      <input name="email" id="textinput-fc" type="text" value="" placeholder="Sähköposti" required>
+      <input name="password" id="textinput-fc" type="password" value="" placeholder="Salasana" required>
+      <input type="submit" value="Ok" name="submit">
     </div>
   </form>
 
+<?php
+session_start();
 
+if (isset($_POST['submit'])) {
+  $admins = findAll($admins);
+  foreach ($admins as $admin) {
+    if ($_POST['email'] == $admin['email'] && $_POST['password'] == $admin['password']) {
+        $_SESSION['loggedIn'] = true;
+		$_SESSION['email'] = $admin['email'];
+        header('Location: status.php');
+    }
+	else {
+		  echo "<p style='color: #B22222;font-size: 20px;border-bottom: 1px solid #FFF;padding-bottom: 8px;'>Invalid username or password!</p>";
+		  header("Refresh: 5; url=userManagement.php");
+	  }
+  }
+  
+}
+?>
 </div>
 </div>
 </body>
